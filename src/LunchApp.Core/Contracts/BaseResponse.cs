2768a8374f19461303ab3ee05335b969979c1fc5
kristian <gammel4@gmail.com>
2020-02-22 15:18:15 +0100

    lunch contracts

---
+++ src/LunchApp.Core/Contracts/BaseResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunchApp.Core.Contracts
+{
+    public abstract class BaseResponse
+    {
+        public BaseResponse(bool result, IList<string> meseages)
+        {
+            _ = meseages ?? throw new NullReferenceException("Meseages must not be null ");
+            Result = result;
+            Meseages = meseages; 
+        }
+
+        public bool Result { get; private set;}
+        public  IList<string> Meseages { get; private set; }
+    }
+}
---
+++ src/LunchApp.Core/Contracts/IRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LunchApp.Core.Contracts
+{
+    public interface IRequest
+    {
+    }
+
+    public interface IRequest<out TResponse>
+    {
+    }
+}
