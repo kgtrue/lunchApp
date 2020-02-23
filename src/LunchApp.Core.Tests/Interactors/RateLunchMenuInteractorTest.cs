992d4180f7800f364e71673bece38eca7547171a
Kristian <True>
2020-02-23 11:33:42 +0100

    lunchmenu

---
+++ src/LunchApp.Core.Tests/Interactors/RateLunchMenuInteractorTest.cs
@@ -0,0 +1,29 @@
+﻿using LunchApp.Core.Contracts;
+using LunchApp.Core.Interactors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Moq;
+using LunchApp.Core.Entity;
+
+namespace LunchApp.Core.Tests.Interactors
+{
+    public class RateLunchMenuInteractorTest
+    {
+        private readonly Mock<ILunchMenuRepo> lunchMenuRepo = new Mock<ILunchMenuRepo>();
+        private readonly Mock<ILunchMenuReviewRepo> lunchMenuReviewRepo = new Mock<ILunchMenuReviewRepo>();
+        public RateLunchMenuInteractorTest()
+        {
+            lunchMenuRepo.Setup(x => x.GetById(It.IsAny<int>())).Returns(new LunchMenu(DateTime.Now) { });
+            lunchMenuReviewRepo.Setup(x => x.SaveUpdate(It.IsAny<LunchMenuReview>())).Throws<LunchMenureviewSaveUpdateException>();
+        }
+
+        public void TestRateLunchMenuInteractorFail(int menuId, List<(int DishId, int ReviewScore)> reviews)
+        {
+            var interactor = new RateLunchMenuInteractor(lunchMenuRepo.Object, lunchMenuReviewRepo.Object);
+            var response = interactor.Handle(new Contracts.Dtos.RateLunchMenuRequest(menuId ,reviews));
+            Assert.False(response.Result);
+        }
+    }
+}
---
+++ src/LunchApp.Core/Entity/LunchMenuReview.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace LunchApp.Core.Entity
+{
+    public class LunchMenuReview
+    {
+        public LunchMenuReview(LunchMenu lunchMenu)
+        {
+            _ = lunchMenu ?? throw new NullReferenceException("Lunch menu must not be null");
+            LunchMenu = lunchMenu; 
+            LunchMenuDishReviews = new List<LunchDishReview>();
+        }
+
+        public LunchMenu LunchMenu { get; private set; }
+
+        public IList<LunchDishReview> LunchMenuDishReviews { get; private set; }
+
+        public void AddReview(int lunchDishId, int reviewScore)
+        {
+            if(LunchMenu.LunchDishes.Any(l=> l.)  )
+        }
+    }
+}
